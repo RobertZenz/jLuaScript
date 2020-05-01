df161968bd52a3d5d0b1233bec65a8bceff33171
Robert Zenz <Robert.Zenz@bonsaimind.org>
2020-05-01 15:31:40 +0200

    Added TreeDirFunction.

---
+++ src/org/bonsaimind/jluascript/lua/functions/TreeDirFunction.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020, Robert 'Bobby' Zenz
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>
+ * or write to the Free Software Foundation, Inc., 51 Franklin Street,
+ * Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.bonsaimind.jluascript.lua.functions;
+
+import java.util.Iterator;
+
+import org.bonsaimind.jluascript.lua.system.Coercer;
+import org.luaj.vm2.LuaValue;
+
+public class TreeDirFunction extends DirFunction {
+	public TreeDirFunction(Coercer coercer) {
+		super(coercer);
+	}
+	
+	@Override
+	protected Iterator<?> getIterator(LuaValue luaValue) {
+		return walk(getPath(luaValue).normalize(), Integer.MAX_VALUE);
+	}
+}
